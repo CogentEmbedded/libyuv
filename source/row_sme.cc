ee808cbe35265a71cfce0ca50923709946a5c523
Andrey Vostrikov <andrey.vostrikov@cogentembedded.com>
2025-09-20 11:16:38 +0300

    Exclude SME if SVE is not defined as it uses SVE macro
    Signed-off-by: Andrey Vostrikov <andrey.vostrikov@cogentembedded.com>

--- source/row_sme.cc
+++ source/row_sme.cc
@@ -16,6 +16,8 @@ namespace libyuv {
 extern "C" {
 #endif
 
+#if !defined(LIBYUV_DISABLE_SVE) && defined(__aarch64__)
+
 #if !defined(LIBYUV_DISABLE_SME) && defined(CLANG_HAS_SME) && \
     defined(__aarch64__)
 
@@ -1117,6 +1119,7 @@ __arm_locally_streaming void ARGBToYMatrixRow_SME(
 
 #endif  // !defined(LIBYUV_DISABLE_SME) && defined(CLANG_HAS_SME) &&
         // defined(__aarch64__)
+#endif // !defined(LIBYUV_DISABLE_SVE) && defined(__aarch64__)
 
 #ifdef __cplusplus
 }  // extern "C"
